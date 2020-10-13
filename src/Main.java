3676f100d1111607f7d6174e404b4b32f6e20df3
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-13 17:50:04 +0200

    rozwiązanie

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-
+    public static void main(String[] args) {
+        Televisor tv = new Televisor();
+        System.out.println(tv.showStatus());
+        tv.turnOn(true);
+        System.out.println(tv.showStatus());
+        tv.turnOn(false);
+        System.out.println(tv.showStatus());
+    }
 }
--- src/Televisor.java
+++ src/Televisor.java
@@ -1,3 +1,15 @@
 public class Televisor {
+    private boolean status = false;
 
+    public void turnOn(boolean status){
+        this.status = status;
+    }
+
+    public void turnOff(boolean status){
+        this.status = status;
+    }
+
+    public boolean showStatus() {
+        return status;
+    }
 }
